9408a6b591913586a1b76a5e461d5e1ea631dac3
flipchan <flipchan@stackb0x>
2017-08-15 21:38:33 +0100

    code - code - code

--- gbgcoin.sol
+++ gbgcoin.sol
@@ -1,2 +1,16 @@
 pragma solidity ^0.4.11;
-string public constant url = "https://darksky.net/forecast/57.7072,11.967/si24/en";
+
+//
+//	Gothenburg Coin a ethereum contract based on the weather
+//
+
+contract gbgcoin {
+
+
+	function get_url_data{
+		string public constant url = "https://darksky.net/forecast/57.7072,11.967/si    24/en";
+			}
+
+}
+
+
