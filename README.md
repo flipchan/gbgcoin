02b672ab415a87b1234f33f823ef52cd2f2a90df
root <root@stackb0x>
2017-11-12 16:45:54 +0000

    Wow! new code!, did some more coding on this

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # gbgcoin
 this is a fork of https://github.com/Anders-Linden 's claes coin that he talked about during a meetup
 
+##GothenBurg Coin
+a ethereum token | bet on amount it rains in gothenburg
+
+all dev is welcome
+
+
+
 
 fork of https://github.com/ethgbg/claes.cash
 
 gbgcoin stands for gothenburg coin and is a ethereum contract based on the current weather in gothenburg
+
+
+#Meet n talk
+Come to ethgbg
+https://www.meetup.com/Ethereum-Gothenburg/
+
--- gbgcoin.sol
+++ gbgcoin.sol
@@ -1,16 +1,72 @@
 pragma solidity ^0.4.11;
 
-//
-//	Gothenburg Coin a ethereum contract based on the weather
-//
+//gbg coin | bet on how much its gonna rain in gothenburg
+//send out coins to winner ones a week 
+// this is still in dev, dont use it yet, all contribution code is welcome
 
-contract gbgcoin {
+/*
+ * --------------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <flipchan@riseup.net> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Filip (flipchan) Kalebo
+ * ---------------------------------------------------------------------------------
+ */
 
 
-	function get_url_data{
-		string public constant url = "https://darksky.net/forecast/57.7072,11.967/si    24/en";
-			}
+contract GothenBurgCoin {
 
+    	string public constant symbol = "GBGCOIN";
+    	string public constant name = "Gothenburg Coin";
+    	uint8 public constant decimals = 18;
+
+	string public payday = 'sunday';
+
+	event UpdateStatus(string meddelande, uint _amount); //public print
+	
+	//guesser directory
+	mapping(address => Guesser) public guessers;
+
+	Guess[] public guesses;	//lets make the guesses countable
+
+	weathermap[monday, tuesday, wednesday, thursday, friday, saturday, sunday] public weatherdays; // set item to true if it has rained that day and then count them to generate the output
+
+	struct weatherguess{
+		uint amountdays;
+		address guesser;
+	}	//how many days is it gonna rain
+
+	modifier ifPayday(){	//check if it is the correct day to hand out the coin and if we got coin to hand out
+        	if(currentday != payday && getFunds != 0){
+            		throw;
+        	}else{
+        	    _;
+       		 }
+    		}	
+
+	function whatdayisit() returns(string){//return today
+		return today;
+		}
+
+	function depositfunds() payable { //add input for guess
+		UpdateStatus("GbgCoin inserted!", msg.sender, msg.value);
+		}
+
+	function calculateweather(){
+		return weather;
+	} 
+
+
+
+	function paywinner(uint amount) ifPayday{
+
+		}
+
+	function getFunds() constant returns(uint){
+        	return this.balance;
+    		}
+
+	function getcurrentWeather() constant returns(bool){//easiest to check day as a boolean
+		return calculateweather("https://darksky.net/forecast/57.7072,11.967/si24/en");
+		}
 }
-
-
